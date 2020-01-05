9c9ecba7ebae562c6cc2d58871d3a72011730d88
Mr-Martini <martini@criptext.com>
2020-01-05 20:12:43 -0300

    started home development

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,10 +1,37 @@
-import { Paper } from '@material-ui/core'
+import { Paper, Container, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
 import React from 'react'
 
-export default function() {
+const useStyles = makeStyles(theme => ({
+    paper: {
+        backgroundColor: '#4d4d4d',
+        color: 'white',
+        marginTop: '2em',
+        padding: '1em',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems:"center",
+    },
+    text: {
+        marginTop: '1em',
+        marginBottom: '1em',
+    },
+}))
+
+export default function () {
+
+    const classes = useStyles()
+
     return (
-        <Paper>
-            Home
-        </Paper>
+        <Container maxWidth='lg'>
+            <Paper elevation={12} className={classes.paper}>
+                <Typography>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur quam tortor, suscipit vitae suscipit quis, sodales ac elit. Sed pulvinar dolor ipsum, vitae rutrum tortor fermentum a. Praesent neque massa, malesuada vestibulum mi eu, suscipit pharetra felis. Proin condimentum luctus dui, ac tristique diam dignissim vel.
+                </Typography>
+                <Typography className={classes.text} color='secondary' variant='h3'>
+                    Symath
+                </Typography>
+            </Paper>
+        </Container>
     )
 }
