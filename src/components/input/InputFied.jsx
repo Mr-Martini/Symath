df59ab99356e766b7cfcb1bdb803ea7599dba25d
Mr-Martini <martini@criptext.com>
2020-01-06 23:37:37 -0300

    updated home input field

--- src/components/input/InputFied.jsx
+++ src/components/input/InputFied.jsx
@@ -1,72 +1,75 @@
 import React from 'react'
 import { makeStyles, withStyles } from '@material-ui/core/styles';
-import {TextField} from '@material-ui/core'
+import { TextField } from '@material-ui/core'
 import InputAdornment from '@material-ui/core/InputAdornment';
 import SearchIcon from '@material-ui/icons/Search'
 
 const useStyles = makeStyles(theme => ({
-    textField: {
-        marginLeft: theme.spacing(1),
-        marginRight: theme.spacing(1),
-        marginBottom: theme.spacing(1),
-        width: "60%",
-        borderRadius: '0.4em',
-    },
-    input: {
-      color: 'white'
-    }
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+    width: "60%",
+    borderRadius: '0.4em',
+  },
+  input: {
+    color: 'white'
+  }
 }));
 
 const CssTextField = withStyles({
-    root: {
-      '& label.Mui-focused': {
-        color: 'lightgreen',
+  root: {
+    '& label.Mui-focused': {
+      color: 'lightgreen',
+    },
+    '& .MuiInput-underline:after': {
+      borderBottomColor: 'green',
+    },
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: '#F50057',
       },
-      '& .MuiInput-underline:after': {
-        borderBottomColor: 'green',
+      '&:hover fieldset': {
+        borderColor: 'yellow',
       },
-      '& .MuiOutlinedInput-root': {
-        '& fieldset': {
-          borderColor: '#F50057',
-        },
-        '&:hover fieldset': {
-          borderColor: 'yellow',
-        },
-        '&.Mui-focused fieldset': {
-          borderColor: 'green',
-        },
+      '&.Mui-focused fieldset': {
+        borderColor: 'green',
       },
     },
-  })(TextField);
+  },
+})(TextField);
 
 
-export default function ({pegarInput}) {
+export default function ({ pegarInput, id, label, type, margin, placeholder, icone }) {
 
-    const classes = useStyles()
+  const classes = useStyles()
 
-    return (
-        <CssTextField
-            InputLabelProps={{
-                className: classes.input
-            }}
-            id="standard-search"
-            label="Data"
-            type="search"
-            className={classes.textField}
-            margin="normal"
-            placeholder="Split the numbers by ; e.g(4;4.69;7.77;5)"
-            onChange={pegarInput}
-            color='secondary'
-            variant='outlined'
-            InputProps={{
-                className: classes.input,
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <SearchIcon />
-                  </InputAdornment>
-                ),
-              }}
-        />
-    )
+  return (
+    <CssTextField
+      InputLabelProps={{
+        className: classes.input
+      }}
+      id={id}
+      label={label}
+      type={type}
+      className={classes.textField}
+      margin={margin}
+      placeholder={placeholder}
+      onChange={pegarInput}
+      color='secondary'
+      variant='outlined'
+      InputProps={{
+        className: classes.input,
+        startAdornment: (
+          <InputAdornment position="start">
+            {icone === 'search' ?
+              <SearchIcon /> : null
+            }
+          </InputAdornment>
+        ),
+      }}
+
+    />
+  )
 
 }
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -12,7 +12,7 @@ const useStyles = makeStyles(theme => ({
         padding: '1em',
         display: 'flex',
         flexDirection: 'column',
-        alignItems:"center",
+        alignItems: "center",
     },
     text: {
         marginTop: '1em',
@@ -23,7 +23,7 @@ const useStyles = makeStyles(theme => ({
 
 export default function () {
 
-    const [pureInput, setPureInput ] = useState()
+    const [pureInput, setPureInput] = useState()
 
     const classes = useStyles()
 
@@ -37,15 +37,23 @@ export default function () {
         <Container maxWidth='lg'>
             <Paper elevation={12} className={classes.paper}>
                 <Typography>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur quam tortor, suscipit vitae suscipit quis, sodales ac elit. Sed pulvinar dolor ipsum, vitae rutrum tortor fermentum a. Praesent neque massa, malesuada vestibulum mi eu, suscipit pharetra felis. Proin condimentum luctus dui, ac tristique diam dignissim vel.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur quam tortor, suscipit vitae suscipit quis, sodales ac elit. Sed pulvinar dolor ipsum, vitae rutrum tortor fermentum a. Praesent neque massa, malesuada vestibulum mi eu, suscipit pharetra felis. Proin condimentum luctus dui, ac tristique diam dignissim vel.
                 </Typography>
                 <Typography className={classes.text} color='secondary' variant='h3'>
                     Symath
                 </Typography>
-                <InputField pegarInput={pegarInput}/>
-                { pureInput ?
-                <Button color='secondary' variant='contained' >Calculate</Button>
-                : null}
+                <InputField
+                    pegarInput={pegarInput}
+                    id='standard-search'
+                    label='Data'
+                    type='search'
+                    margin='normal'
+                    placeholder='Split the numbers by ; e.g(4;4.69;7.77;5)'
+                    icone='search'
+                />
+                {pureInput ?
+                    <Button color='secondary' variant='contained' >Calculate</Button>
+                    : null}
             </Paper>
         </Container>
     )
