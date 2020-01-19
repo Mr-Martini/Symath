342abb6090a50c9048cc79d55e4488aadba2e078
Mr-Martini <martini@criptext.com>
2020-01-19 20:34:49 -0300

    fixed switch button error

--- src/Redux/Switch/SwitchActions.js
+++ src/Redux/Switch/SwitchActions.js
@@ -1,12 +1,8 @@
 import HANDLE_SWITCH from './SwitchActionTypes'
 
-export const sagaSwitch = () => ({
-    type: HANDLE_SWITCH.TOGGLE_SWITCH
-})
-
 export const toggleSwitchButton = (name, e) => {
     return {
-    type: HANDLE_SWITCH.SET_SWITCH,
+    type: HANDLE_SWITCH.TOGGLE_SWITCH,
     event: e.target,
     name: name
     }
